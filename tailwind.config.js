f6735e9482537f71a12dc433ed11838792ca20b5
Rizalramzi <rizalramzi0@gmail.com>
2024-09-10 06:09:40 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        dark : '#101415',
+        text_dark : '#1E1F20'
+      }
+    },
+  },
+  plugins: [],
+}
+
